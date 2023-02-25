2bbabc04243634dace14a9fe5d52d0540570b975
pcgomesp <pcesarpgomes@gmail.com>
2023-02-25 10:12:23 -0300

    corrige erro na solicitação do reset de senha

--- guest/urls.py
+++ guest/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('auth/logout', views.user_logout, name='user_logout'),
 
     # rotas de recuperação de senha    
-    path('password_reset', views.password_reset, name='password_reset'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="guest/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="guest/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="guest/new_password.html"), name="password_reset_confirm"),
