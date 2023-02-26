93c2ca2d39ec3f82e1a717dc8d1f8f10e9d55aec
pcgomesp <pcesarpgomes@gmail.com>
2023-02-26 16:37:20 -0300

    adiciona view com dados do back

--- owner/views.py
+++ owner/views.py
@@ -6,11 +6,11 @@ from django.forms.models import model_to_dict
 from django.views.decorators.csrf import csrf_exempt
 import random
 import json
-from guest.models import User, Establishment, Plate, Order, OrderItem, Address
+from guest.models import User, Establishment, Plate, Order, OrderItem, Address, Menu
 from django.core.paginator import Paginator
 
 def index(request):
-	return redirect('owner:establishment_products')
+	return redirect('owner:establishment_products2')
 
 def establishment_details(request):
 	return render(request, 'owner/establishment-signup.html')
@@ -19,7 +19,19 @@ def add_product(request):
 	return render(request, 'owner/components/add-product-modal.html')
 
 def establishment_products(request):
-	return render(request, 'owner/pages/products.html')
+	mainEstablishment = request.user.establishment_id
+	menus = Menu.objects.filter(establishment_id=mainEstablishment.establishment_id).all()
+	aux = []
+	for menu in menus:
+		aux += Plate.objects.filter(menu_id=menu.menu_id).all()
+	#menus.plates = aux
+	#page
+	paginator = Paginator(aux, 6) # Mostra até 6 pedidos por página
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+	
+	context = {'page_obj': page_obj}
+	return render(request, 'owner/pages/products.html', context)
 
 def order_history(request):
 	mainEstablishment = request.user.establishment_id #daqui tá vindo o estabelecimento já de ctz
