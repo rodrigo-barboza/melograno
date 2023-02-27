bdd84be117d4381d0be5235e59936b0e491eb1c3
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2023-02-27 09:39:51 -0300

    feat: corrige venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sysvale/Documentos/projects/sd/web-folder/melograno/melograno"
+set -gx VIRTUAL_ENV "/home/sysvale/Documentos/projects/sd/web-folder/melograno/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(melograno) "
-            printf "%s%s" "(melograno) " (set_color normal)
+        if test -n "(venv) "
+            printf "%s%s" "(venv) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
