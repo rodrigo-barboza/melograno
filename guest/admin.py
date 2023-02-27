18c37edaf841cdd0ef4aca559d7ed080044f4adf
pcgomesp <pcesarpgomes@gmail.com>
2023-02-26 21:29:10 -0300

    organiza um pouco o espaço do admin

--- guest/admin.py
+++ guest/admin.py
@@ -2,23 +2,63 @@ from django.contrib import admin
 from .models import User, Establishment, Address, Menu, Order, Plate, Cart, CartItem, OrderItem
 # Register your models here.
 class UserAppAdmin(admin.ModelAdmin):
-    pass
-
+    date_hierarchy = 'last_login'
+    list_display = ('username', 'email', 'role', 'howManyOrders')
+    @admin.display(description='Total de pedidos feitos')
+    def howManyOrders(self, obj):
+        if(obj.role == 'client'):
+            order_list = Order.objects.filter(user_id=obj.user_id).all()
+            return len(order_list)
+        else:
+            return 0
 
 class EstablishmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'cnpj', 'categories', 'howManySales')
+
+    @admin.display(description='Total de pedidos vendidos')
+    def howManySales(self, obj):
+        return len(Order.objects.filter(establishment_id = obj.establishment_id))
 
 class AddressAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('street', 'district', 'number')
 
 class MenuAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('stablishmentName',)
+
+    @admin.display(description='Estabelecimento referente')
+    def stablishmentName(self, obj):
+        return Establishment.objects.get(establishment_id = obj.establishment_id.establishment_id).name
 
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('receiver','status','street', 'district', 'value')
+
+    @admin.display(description='Recebedor')
+    def receiver(self,obj):
+        return obj.user_id 
+    
+    @admin.display(description='Rua')
+    def street(self,obj):
+        return obj.address_id.street 
+    
+    @admin.display(description='Bairro')
+    def district(self,obj):
+        return obj.address_id.district
+    
+    @admin.display(description='Valor do Pedido (R$)')
+    def value(self,obj):
+        return obj.price
 
 class PlateAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'value', 'category', 'whereFrom')
+
+    @admin.display(description='Valor do Prato (R$)')
+    def value(self,obj):
+        return obj.price
+    
+    @admin.display(description='Estabelecimento referente')
+    def whereFrom(self, obj):
+        return Establishment.objects.get(establishment_id = obj.menu_id.establishment_id.establishment_id).name
+    
 
 class CartAdmin(admin.ModelAdmin):
     pass
@@ -27,7 +67,15 @@ class CartItemAdmin(admin.ModelAdmin):
     pass
 
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('quantity','whatAddressee','whatPlate',)
+
+    @admin.display(description='Recebedor')
+    def whatAddressee(self,obj):
+        return obj.order_id.user_id
+
+    @admin.display(description='Prato Pedido')
+    def whatPlate(self,obj):
+        return obj.plate_id.name
 
 admin.site.register(User, UserAppAdmin)
 admin.site.register(Establishment, EstablishmentAdmin)
